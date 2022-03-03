0f184cac95a20d07c70a274e95ecb2f269d84b02
Devan <deshlock010101@hotmai.com>
2022-03-03 09:34:46 -0600

    completed exercise

--- PracticeLinq/Program.cs
+++ PracticeLinq/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PracticeLinq
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var videoGames = new List<string>() { "God of War", "Stranger Things", "Halo", "Call of Duty"};
+            var videoGamesLength = videoGames.OrderByDescending(x => x.Length);
+            foreach(var game in videoGamesLength)
+            {
+                Console.WriteLine(game);
+            }
+            
         }
     }
 }
